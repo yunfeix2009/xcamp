2bb3e46a806466a93a6c487a446a3930d1a3e78a
yunfeix2009 <yunfeix2009@163.com>
2024-09-29 21:43:50 -0700

    init2

--- C202/C202L01HW02/counting_rooms_description.md
+++ C202/C202L01HW02/counting_rooms_description.md
@@ -1,2 +1,51 @@
-#  <#Title#>
+2.  Counting Rooms
+Problem ID: 14066Required Problem100pt(s)
+The Latest:
+Accepted
+100 pt(s)
+The Best:
+Accepted
+100 pt(s)
+Time Limit: 2000ms
+Memory Limit: 262144kB
+Description
+Time limit: 1.00 s
 
+Memory limit: 256 MB
+
+You are given a map of a building made up of 
+n
+×
+m
+n×m squares, and each square is either a floor or a wall. Find how many rooms are in the building. A room is defined by a group of any number of adjacently connected floor squares, that is surrounded by walls.
+
+Input
+The first input line has two integers 
+n
+n and 
+m
+m: the height and width of the map.
+
+Then there are 
+n
+n lines each containing 
+m
+m characters describing the map. Each character is either . (floor) or # (wall).
+
+Output
+Print one integer, the number of rooms.
+
+Constraints
+1≤n,m≤1000
+
+Sample Input:
+5 8
+
+########
+#..#...#
+####.#.#
+#..#...#
+########
+
+Output:
+3
--- C202/C202L01HW02/main.cpp
+++ C202/C202L01HW02/main.cpp
@@ -1,14 +1,75 @@
-//
-//  main.cpp
-//  C202L01HW02
-//
-//  Created by Yunfei Xia on 9/29/24.
-//
+#include<iostream>
+using namespace std;
+const int mx_n = 1000;
+char maze[mx_n][mx_n];
+bool visted[mx_n][mx_n];
+int r, c, room_num;
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+// define "impossible" to smooth the code
+bool impossible(int x, int y){
+    return x<0 || y<0 || x>=r || y>=c || maze[x][y] == '#' || visted[x][y];
 }
+
+//fill in the room that's newly discovered
+void floodfill(int x, int y){
+    if (impossible(x, y)) return;
+    visted[x][y] = true;
+    //visit the neighbors
+    floodfill(x, y+1);
+    floodfill(x, y-1);
+    floodfill(x+1, y);
+    floodfill(x-1, y);
+}
+
+//check if current position is a new room, if so, fill in the room
+void count_rooms(int x, int y){
+    if (impossible(x, y)) return;
+    room_num++;
+    floodfill(x, y);
+    return;
+}
+
+int main(){
+    // read input
+    room_num = 0;
+  cin >> r >> c;
+  for (int i=0; i<r; i++){
+    for (int j = 0; j<c; j++){
+      cin >> maze[i][j];
+    }
+  }
+    //loop the maze and count the room at each position
+    for (int i=0; i<r; i++){
+    for (int j = 0; j<c; j++){
+        count_rooms(i, j);
+    }
+  }
+    cout << room_num;
+  return 0;
+}
+
+/*
+10 10
+#..#.##..#
+#.#####.#.
+####.####.
+...#.#.###
+#######...
+#..#.###.#
+.#.#.#####
+###.#.#...
+#..###.##.
+###...##.#
+ 
+ output: 18
+ 
+ Sample Input:
+ 5 8
+ ########
+ #..#...#
+ ####.#.#
+ #..#...#
+ ########
+ Output:
+ 3
+*/
