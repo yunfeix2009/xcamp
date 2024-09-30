bb7b61bb75992e489e1a46dba0dea17480fae3bf
yunfeix2009 <yunfeix2009@163.com>
2024-09-29 21:43:00 -0700

    init

---
+++ C202/C202L01HW02/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C202L01HW02
+//
+//  Created by Yunfei Xia on 9/29/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
